e3d55ee5e295350351c9554b1ffbbf28a9a80f70
Hugo Müller-Downing <github@hu.md>
2021-06-27 11:49:06 +1000

    Add type specs to card and deck

--- lib/five_hundred/card.ex
+++ lib/five_hundred/card.ex
@@ -1,15 +1,22 @@
 defmodule FiveHundred.Card do
-  """
-  @doc
-  Cards have a rank and a suit.
+  @doc """
+  Models a card from a standard 53 card deck
   """
 
   @derive Jason.Encoder
   defstruct [:rank, :suit]
-
   alias FiveHundred.Card
 
+  @type t :: %Card{rank: rank, suit: suit}
+  @type suit :: :hearts | :diamonds | :clubs | :spades | :joker
+  @type rank :: 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15
+
+  @spec suits() :: [suit]
   def suits(), do: [:hearts, :diamonds, :clubs, :spades]
+
+  @spec ranks() :: [rank]
   def ranks(), do: Enum.to_list(2..14)
+
+  @spec joker() :: %Card{rank: rank, suit: suit}
   def joker(), do: %Card{rank: 15, suit: :joker}
 end
--- lib/five_hundred/deck.ex
+++ lib/five_hundred/deck.ex
@@ -1,17 +1,25 @@
 defmodule FiveHundred.Deck do
+  @moduledoc """
+  Models a four-handed 500 deck
   """
-  @doc
-  A four handed 500 deck
-  """
-
   alias FiveHundred.{Deck, Card}
 
+  @type t :: [Card.t()]
+
+  @spec new_shuffled() :: Deck.t()
+  @doc """
+  new_shuffled/0: returns a new shuffled four-handed deck
+  """
   def new_shuffled() do
     new_deck()
     |> Enum.filter(&four_handed_filter/1)
     |> Enum.shuffle()
   end
 
+  @spec new_deck() :: Deck.t()
+  @doc """
+  new_deck/0: creates a new 53 card deck, including the joker
+  """
   defp new_deck() do
     for suit <- Card.suits(), rank <- Card.ranks() do
       %Card{rank: rank, suit: suit}
@@ -19,6 +27,12 @@ defmodule FiveHundred.Deck do
     |> Enum.concat([Card.joker()])
   end
 
+  @spec four_handed_filter(Card.t()) :: boolean
+  @doc """
+  four_handed_filter/1: given a card, returns a boolean indicating
+  whether it should be included for a four-handed deck.
+  Filters red fours down exclusive.
+  """
   defp four_handed_filter(%Card{suit: suit, rank: rank} = card)
        when rank <= 3,
        do: false
