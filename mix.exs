519058cda30b6a815dd37c425e1341d7104d30be
Hugo Müller-Downing <github@hu.md>
2023-01-03 10:57:11 +1100

    Fixes libcluster failing to find other nodes

--- mix.exs
+++ mix.exs
@@ -61,7 +61,7 @@ defmodule FiveHundred.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:swoosh, "~> 1.3"},
       {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"},
+      {:telemetry_poller, "~> 1.0"}
     ]
   end
 
